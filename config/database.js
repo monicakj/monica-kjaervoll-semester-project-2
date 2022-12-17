b6ac7c05270244476b55213da4ac11383470850e
monicakj <79156195+monicakj@users.noreply.github.com>
2022-12-17 05:27:45 +0100

    yarn build

--- config/database.js
+++ config/database.js
@@ -24,18 +24,18 @@ module.exports = ({ env }) => {
 };
 
 // Use this configuration for an SQLite database on your machine.
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
